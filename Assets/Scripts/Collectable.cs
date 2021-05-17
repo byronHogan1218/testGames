79e30144fefe79415eaa98607812dee1bd57a9bd
byronHogan1218 <byronhogan1218@gmail.com>
2021-05-17 18:48:23 -0500

    Adding main menu, fixing some code, making constants

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -3,16 +3,16 @@
 public class Collectable : MonoBehaviour {
     private CollectableManager collectableManager;
     void Start() {
-        collectableManager = GameObject.FindGameObjectWithTag("CollectableManager").GetComponent<CollectableManager>();
+        collectableManager = GameObject.FindGameObjectWithTag(Constants.COLLECTABLE_MANAGER_TAG).GetComponent<CollectableManager>();
         if (collectableManager == null) {
             print("Collectable Manager missing from Scene. Please add from prefabs");
         }
     }
 
     private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("Player")) {
+        if (other.gameObject.CompareTag(Constants.PLAYER_TAG)) {
             //The player has picked this up
-            collectableManager.addOne();
+            collectableManager.AddOne();
             Destroy(this.gameObject);
         }
     }
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public void LoadLevelSelector() {
+        SceneManager.LoadScene(Constants.LEVEL_SELECTOR_SCENE);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
